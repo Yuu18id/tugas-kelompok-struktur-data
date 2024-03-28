9c22cc6e568ba00133096c88a9a339eebcfb1a8c
Dev-Hisaki <muharuichijou@gmail.com>
2024-03-28 22:04:39 +0700

    Add : Stack task list function

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Stack stack = new Stack(7);
+        stack.push("Sate Ayam");
+        stack.push("Sate Kambing");
+        stack.push("Sate Sapi");
+        stack.printStack();
+        stack.pop();
+        stack.printStack();
     }
 }
